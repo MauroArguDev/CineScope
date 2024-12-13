ef98aa4f83168dd0dd2d882ce8b41515586bae09
Mauricio Argumedo <mauargudev@gmail.com>
2024-12-13 16:50:12 -0600

    Project creation

---
+++ CineScope/CineScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineScopeApp.swift
+//  CineScope
+//
+//  Created by Mauricio Argumedo on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CineScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineScope/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CineScope
+//
+//  Created by Mauricio Argumedo on 13/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
