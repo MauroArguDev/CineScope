b25a0e4e33318287d7e58f5e1d7bbf73781cc224
Mauricio Argumedo <mauargudev@gmail.com>
2024-12-15 13:22:33 -0600

    Lottie Animation implemented
    Lottie package implemented
    Splash animation added to project
    Splash Screen created

---
+++ CineScope/SplashScreen.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CineScope
+//
+//  Created by Mauricio Argumedo on 13/12/24.
+//
+
+import SwiftUI
+import Lottie
+
+struct SplashScreen: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                LottieView(animation: .named("splash"))
+                    .playing(loopMode: .playOnce)
+                    .animationSpeed(1.2)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
